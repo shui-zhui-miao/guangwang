770f171e1a6b1372c4d07978df79a86f7cb27c38
BrownMing <112614329+laoxuao@users.noreply.github.com>
2025-04-03 18:15:02 +0800

    3

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,5 +7,10 @@ defineOptions({
 <template>
   <div>
     <img h-full w-full src="../assets/bg.png" alt="">
+    <div class="email" absolute bottom-10 w-full flex justify-center>
+      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
+        gongdenghua0925@icloud.com
+      </div>
+    </div>
   </div>
 </template>
