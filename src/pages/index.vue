9932260a48d5f85a363041b802fc1779efee8959
BrownMing <112614329+laoxuao@users.noreply.github.com>
2025-08-29 11:16:34 +0800

    1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,7 +6,7 @@ defineOptions({
 
 <template>
   <div>
-    <img h-full w-full src="../assets/bg.webg" alt="">
+    <img h-full w-full src="../assets/bg.webp" alt="">
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
 
