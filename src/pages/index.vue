c975cdf7cc86c25a98d099cfa037553f71213c31
BrownMing <112614329+laoxuao@users.noreply.github.com>
2025-11-14 14:09:28 +0800

    1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,10 +6,10 @@ defineOptions({
 
 <template>
   <div>
-    <img h-full w-full src="../assets/bg.png" alt="">
+    <img h-full w-full src="../assets/bg.webp" alt="">
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        wulijun2002120822@icloud.com
+        
       </div>
     </div>
   </div>
--- src/pages/privacy.vue
+++ src/pages/privacy.vue
@@ -2,7 +2,7 @@
 defineOptions({
   name: 'IndexPage',
 })
-const appName = 'Tunx'
+const appName = 'Bayo'
 </script>
 
 <template>
